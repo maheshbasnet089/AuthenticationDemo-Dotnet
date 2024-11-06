10f5cf438452ed65f45b2a6b53eb24c4d0d5d759
maheshbasnet089 <basnetmanish089@gmail.com>
2024-11-06 13:50:34 +0545

    add auth swagger

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -38,7 +39,24 @@ builder.Services.AddAuthentication(options=>{
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c=>{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme{
+        Description = "JWT Authorization header using the Bearer scheme", 
+        Name = "Authorization", 
+        In = ParameterLocation.Header, 
+        Type = SecuritySchemeType.ApiKey, 
+        Scheme = "Bearer"
+    }); 
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement{{
+        new OpenApiSecurityScheme{
+            Reference = new OpenApiReference{
+                Type = ReferenceType.SecurityScheme, 
+                Id = "Bearer"
+            }
+        }, 
+        new string[]{}
+    }});
+});
 
 var app = builder.Build();
 app.UseAuthentication(); 
