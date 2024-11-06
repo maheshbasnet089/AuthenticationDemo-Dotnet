d27cde91feb9ac4943ba8a1df2e6905c83410199
maheshbasnet089 <basnetmanish089@gmail.com>
2024-11-06 10:06:49 +0545

    database connection

--- Authentication/AppDbContext.cs
+++ Authentication/AppDbContext.cs
@@ -4,8 +4,5 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 public class AppDbContext(DbContextOptions<AppDbContext> options, IConfiguration configuration):IdentityDbContext<AppUser>(options){
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
-        base.OnConfiguring(optionsBuilder); 
-        optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
-    }
+ 
 }
--- Program.cs
+++ Program.cs
@@ -1,13 +1,47 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options=>options.UseSqlServer(builder.Configuration["ConnectionStrings:DefaultConnection"])); 
+builder.Services.AddIdentityCore<AppUser>().AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication(options=>{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; 
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; 
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options=>{
+    var secret = builder.Configuration["JwtConfig:secret"]; 
+    var issuer = builder.Configuration["JwtConfig:ValidIssuer"]; 
+    var audience = builder.Configuration["JwtConfig:ValidAudiences"]; 
+    if(secret is null || issuer is null || audience is null){
+        throw new ApplicationException("Jwt is not set in the configuration"); 
+    }
+    options.SaveToken = true; 
+    options.RequireHttpsMetadata = false; 
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters(){
+        ValidateIssuer = true, 
+        ValidateAudience = true, 
+        ValidAudience = audience, 
+        ValidIssuer = issuer, 
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret))
+    };
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.UseAuthentication(); 
+app.UseAuthorization();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
